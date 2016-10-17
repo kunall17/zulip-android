23840f0953f32cc6ac9a4118ad78fe4b50a855ff
kunall17 <kunall.gupta17@gmail.com>
2016-10-17 19:33:43 +0530

    Fixes #153: Show in-app new messages notifications snackbar

--- app/src/main/java/com/zulip/android/activities/MessageListFragment.java
+++ app/src/main/java/com/zulip/android/activities/MessageListFragment.java
@@ -484,6 +484,35 @@ public class MessageListFragment extends Fragment implements MessageListener {
         });
     }
 
+    private void loadMessageId(int id) {
+        if (lastMessageId > id) {
+            int index = adapter.getItemIndex(id);
+            if (index != -1) {
+                recyclerView.scrollToPosition(adapter.getItemIndex(id));
+                return;
+            }
+        }
+        AsyncGetOldMessages oldMessagesReq = new AsyncGetOldMessages(this);
+        oldMessagesReq.setCallback(new ZulipAsyncPushTask.AsyncTaskCompleteListener() {
+            @Override
+            public void onTaskComplete(String result, JSONObject jsonObject) {
+                adapter.setFooterShowing(false);
+                loadingMessages = false;
+            }
+
+            @Override
+            public void onTaskFailure(String result) {
+                Toast.makeText(getActivity(), R.string.no_message, Toast.LENGTH_SHORT).show();
+                adapter.setFooterShowing(false);
+                loadingMessages = false;
+            }
+        });
+        adapter.clear();
+        adapter.setFooterShowing(true);
+        loadingMessages = true;
+        oldMessagesReq.execute(id, LoadPosition.BELOW, 0, 100, filter);
+    }
+
     private Boolean listHasMostRecent() {
         return lastMessageId == app.getMaxMessageId();
     }
@@ -494,5 +523,13 @@ public class MessageListFragment extends Fragment implements MessageListener {
 
     private Message getMessageById(int id) {
         return this.messageIndex.get(id);
+    }
+
+    public void showLatestMessages() {
+        if (listHasMostRecent()) {
+            recyclerView.scrollToPosition(adapter.getItemCount() - 1);
+        } else {
+            loadMessageId(app.getMaxMessageId());
+        }
     }
 }
